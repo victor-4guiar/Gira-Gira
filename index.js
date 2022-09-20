a1b59ce7e421f56bfaf10cf359faec9ecbc035c8
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-09-20 20:13:41 -0300

    w

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function loopar(l=1){
 		helice.appendChild(square);
 	}
 }
+
+function wrenhoo(){
+	let song = new Audio('arc.mp3');
+	song.play();
+	let box = document.getElementById('box');
+	box.remove();
+	document.body.style.backgroundColor = 'black';
+	let neko = document.createElement('img');
+	neko.setAttribute('src', 'neco-arc.gif');
+	neko.classList.add('neko', 'aprox');
+	document.getElementById('title').innerHTML = 'W reñoo';
+	document.body.appendChild(neko);
+	setTimeout(()=>{
+		neko.style.width = '250px';
+		neko.style.height = '250px';
+	}, 2 * 500);
+}
